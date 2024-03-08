f0219ae1f33350722c30d5df2299d35c9b2e25a7
Shobindiran <shobindiran@gmail.com>
2024-03-08 21:25:13 +0530

    added few components

---
+++ client/src/pages/CreateRecipies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateRecipies = () => {
+  return (
+    <div>CreateRecipies</div>
+  )
+}
+
+export default CreateRecipies
---
+++ client/src/pages/SavedRecipies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SavedRecipies = () => {
+  return (
+    <div>SavedRecipies</div>
+  )
+}
+
+export default SavedRecipies
