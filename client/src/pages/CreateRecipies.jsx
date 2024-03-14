7aa6aa447615779bac98461f8a9f61df9b245186
Shobindiran <shobindiran@gmail.com>
2024-03-14 19:03:23 +0530

    commit

--- client/src/pages/CreateRecipies.jsx
+++ client/src/pages/CreateRecipies.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const CreateRecipies = () => {
   return (
-    <div>CreateRecipies</div>
+    <section className="create-recipies">
+      <div className="container">
+        <div className="create-recipies-wrapper">
+          <h2>Create Recipe</h2>
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name"/>
+            <label htmlFor="ingredients">Ingredients</label>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea name="instructions" id="instructions"></textarea>
+            <label htmlFor="imageURL">Image URL</label>
+            <input type="text" id="imageURL" name="imageURL"/>
+            <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+            <input type="number" id="cookingTime" name="cookingTime" />
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
